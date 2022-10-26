41e07eea424ed4629c5cc25579f9c1ab2d8594c1
dabeastt <arjeljabuel@gmail.com>
2022-10-26 10:13:52 +0800

    finished

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Text scoreText;
+    private int scoreCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void AddScore()
+    {
+        scoreCount++;
+        scoreText.text = "Score: " + scoreCount;
+    }
+}
---
+++ Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject[] _SpawnObject;
+    float PositionY;
+    private int randomInt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        int rand = Random.Range(0, _SpawnObject.Length);
+        PositionY = Random.Range(4, -4f);
+         this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+         Instantiate(_SpawnObject[rand], transform.position, transform.rotation);
+    }
+}
